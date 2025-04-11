8599f76b602edac09a361e8c189eb11c553c0b32
Ahmad <ahmadhassanminnhas@gmail.com>
2025-04-11 16:31:26 +0500

    Adding UI components

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-btn :label="label" :color="color" :flat="flat" :class="customClass" @click="$emit('click')" />
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: {
+        label: String,
+        color: String,
+        flat: Boolean,
+        customClass: String,
+    },
+    emits: ['click'],
+};
+</script>
